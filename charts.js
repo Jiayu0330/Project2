697b83c31f3ef043f5ba418c87f0844e243fcbfc
Jiayu Jiang <you@example.com>
2019-03-28 16:47:37 -0400

    second svg

--- charts.js
+++ charts.js
@@ -1,10 +1,10 @@
 var dataP = d3.json("classData.json");
 
-var drawBarChart = function(data)
+var drawOverallBarChart = function(data)
 {
   var screen = { //the size of svg
-    width: 800,
-    height: 350
+    width: 700,
+    height: 360
   }
 
   var margins = {
@@ -14,7 +14,7 @@ var drawBarChart = function(data)
     right: 0
   }
 
-  var innerPadding = 10; //space between bars
+  var innerPadding = 6; //space between bars
 
   var width = screen.width - margins.left - margins.right;
   var height = screen.height - margins.top - margins.bottom;
@@ -29,7 +29,7 @@ var drawBarChart = function(data)
                  .domain([0, 100])
                  .range([height, margins.top]);
 
-  var svg = d3.select("svg")
+  var svg = d3.select(".overallBarChart")
               .attr("width", screen.width)
               .attr("height", screen.height)
 
@@ -41,8 +41,8 @@ var drawBarChart = function(data)
      .data(data)
      .enter()
      .append("rect")
-     .attr("x", function(d, i) {return i * barWidth + margins.left;} )
-     .attr("y", function(d) {return yScale(mathFunction(d));} )
+     .attr("x", function(d, i) {return i * barWidth + margins.left;})
+     .attr("y", function(d) {return yScale(mathFunction(d));})
      .attr("width", barWidth - innerPadding)
      .attr("height", function(d) {return height - yScale(mathFunction(d));})
      .attr("fill", function(d, i) {return colors(i)});
@@ -53,21 +53,21 @@ var drawBarChart = function(data)
      .data(data)
      .enter()
      .append("text")
-     .attr("x",function(d,i) {return i * barWidth + margins.left + 3;} )
-     .attr("y",function(d) {return yScale(mathFunction(d)) - 8;} )
+     .attr("x",function(d,i) {return i * barWidth + margins.left + 3;})
+     .attr("y",function(d) {return yScale(mathFunction(d)) - 8;})
      .text(function(d){
        if (mathFunction(d) != 0)
-       {return Math.round(mathFunction(d));} } );
+       {
+         return Math.round(mathFunction(d));
+       }});
 
-  //draw axis
-  var xAxis = d3.axisBottom(xScale);
+  //y-axis
   var yAxis = d3.axisLeft(yScale);
 
   svg.append("g")
      .classed("yAxis", true)
      .call(yAxis)
      .attr("transform", "translate(" + (margins.left - innerPadding) + ",0)");
-
 }
 
 var mathFunction = function(d)
@@ -95,9 +95,80 @@ var mathFunction = function(d)
   return total_grade;
 }
 
+var drawChangingBarChart = function(data)
+{
+  var screen = { //the size of svg
+    width: 700,
+    height: 360
+  }
+
+  var margins = {
+    left: 50,
+    top: 20,
+    bottom: 40,
+    right: 0
+  }
+
+  var innerPadding = 6; //space between bars
+
+  var width = screen.width - margins.left - margins.right;
+  var height = screen.height - margins.top - margins.bottom;
+
+  var barWidth = width/data.length;
+
+  var xScale = d3.scaleLinear()
+                 .domain([0, data.length])
+                 .range([margins.left, width]);
+
+  var yScale = d3.scaleLinear()
+                 .domain([0, 100])
+                 .range([height, margins.top]);
+
+  var svg = d3.select(".changingBarChart")
+              .attr("width", screen.width)
+              .attr("height", screen.height)
+
+  var colors = d3.scaleOrdinal(d3.schemeCategory10)
+
+  svg.append("g")
+     .classed("rect", true)
+     .selectAll("rect")
+     .data(data)
+     .enter()
+     .append("rect")
+     .attr("x", function(d, i) {return i * barWidth + margins.left;})
+     .attr("y", function(d) {return yScale(mathFunction(d));})
+     .attr("width", barWidth - innerPadding)
+     .attr("height", function(d) {return height - yScale(mathFunction(d));})
+     .attr("fill", function(d, i) {return colors(i)});
+
+  svg.append("g")
+     .classed("textAbove", true)
+     .selectAll("text")
+     .data(data)
+     .enter()
+     .append("text")
+     .attr("x",function(d,i) {return i * barWidth + margins.left + 3;})
+     .attr("y",function(d) {return yScale(mathFunction(d)) - 8;})
+     .text(function(d){
+       if (mathFunction(d) != 0)
+       {
+         return Math.round(mathFunction(d));
+       }});
+
+  //y-axis
+  var yAxis = d3.axisLeft(yScale);
+
+  svg.append("g")
+     .classed("yAxis", true)
+     .call(yAxis)
+     .attr("transform", "translate(" + (margins.left - innerPadding) + ",0)");
+}
+
 dataP.then(function(data)
 {
-  drawBarChart(data); //Final grade of each penguin
+  drawOverallBarChart(data); //Final grade of each penguin
+  drawChangingBarChart(data);
 },
 function(err)
 {
