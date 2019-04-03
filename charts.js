061e64e48d68c5b0be8fd19a5d1d6736ac809e93
Xapakas <47224657+Xapakas@users.noreply.github.com>
2019-04-03 14:37:39 -0400

    added pengarray, temporarily global

--- charts.js
+++ charts.js
@@ -1,5 +1,11 @@
 var dataP = d3.json("classData.json");
 
+var pengArray = ["Bookworm Penguin","Crafty Penguin","Cyclist Penguin","Drunken Penguin", "Easter Penguin",
+                 "EBook Penguin","Farmer Penguin","Gentleman Penguin","Judo Penguin","Moana Penguin",
+                 "Painter Penguin", "Grill Penguin", "Pharaoh Penguin", "Pilot Penguin","Pinga Corr",
+                 "Pixie Penguin","Sailor Penguin", "Santa Penguin", "Santa Penguin", "Tauch Penguin",
+                 "Tux Penguin", "Valentine Penguin","Valentine Penguin Ocal","Wizard Penguin"]
+
 var drawOverallBarChart = function(data)
 {
   var screen = { //the size of svg
